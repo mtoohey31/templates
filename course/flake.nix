5c151a840a4eb77cfa34b2454b0d9e625fcfb932
Matthew Toohey <contact@mtoohey.com>
2022-04-27 13:40:10 -0400

    feat: add latex, pandoc, and taskmatter to course template

--- course/flake.nix
+++ course/flake.nix
@@ -4,11 +4,27 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    taskmatter = {
+      url = "github:mtoohey31/taskmatter";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "utils";
+    };
   };
 
-  outputs = { self, nixpkgs, utils }:
+  # TODO: figure out how to add lf cards keybind
+  outputs = { self, nixpkgs, utils, taskmatter }:
     utils.lib.eachDefaultSystem (system:
-      with import nixpkgs { inherit system; }; {
-        devShell = mkShell { nativeBuildInputs = [ nodePackages.cspell ]; };
+      with import nixpkgs { inherit system; overlays = [ taskmatter.overlay ]; }; {
+        devShell = mkShell {
+          nativeBuildInputs = [
+            nodePackages.cspell
+            # TODO: wrap pandoc with defaults
+            pandoc
+            pkgs.taskmatter
+            (pkgs.texlive.combine {
+              inherit (pkgs.texlive) scheme-small mdframed needspace zref;
+            })
+          ];
+        };
       });
 }
