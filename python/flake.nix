e24a691c5d554e9cabfa4093ecdd1ac693326e1d
Matthew Toohey <contact@mtoohey.com>
2022-06-04 18:21:20 -0400

    feat: add poetry and python-lsp-server to python template devShell

--- python/flake.nix
+++ python/flake.nix
@@ -8,20 +8,26 @@
   };
 
   outputs = { self, nixpkgs, utils, poetry2nix }:
-    utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ poetry2nix.overlay ];
-      }; in
-      with pkgs; rec {
-        packages.default = pkgs.poetry2nix.mkPoetryApplication {
-          projectDir = ./.;
-        };
+    utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ poetry2nix.overlay ];
+        }; in
+        with pkgs; rec {
+          packages.default = pkgs.poetry2nix.mkPoetryApplication {
+            projectDir = ./.;
+          };
 
-        devShells.default = pkgs.poetry2nix.mkPoetryEnv {
-          projectDir = ./.;
-        };
-      }) // {
+          devShells.default = (pkgs.poetry2nix.mkPoetryEnv {
+            projectDir = ./.;
+          }).overrideAttrs (oldAttrs: {
+            nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
+              poetry
+              python3Packages.python-lsp-server
+            ];
+          });
+        }) // {
       overlays.default = nixpkgs.lib.composeManyExtensions [
         poetry2nix.overlay
         (final: _: { CHANGEME = self.packages."${final.system}".default; })
