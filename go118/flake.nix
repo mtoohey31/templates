82b68ac58be64666c66e5573460f2d11ed9b32dd
Matthew Toohey <contact@mtoohey.com>
2022-06-12 15:48:37 -0400

    feat: add gow to go templates

--- go118/flake.nix
+++ go118/flake.nix
@@ -4,20 +4,36 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    gow-src = {
+      url = "github:mitranim/gow";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-        with import nixpkgs { inherit system; }; rec {
-          packages.default = buildGo118Module rec {
-            name = "CHANGEME";
-            pname = name;
-            src = ./.;
-            vendorSha256 = "";
-          };
+  outputs = { self, nixpkgs, utils, gow-src }:
+    utils.lib.eachDefaultSystem
+      (system: with import nixpkgs
+        {
+          overlays = [
+            (final: prev: {
+              gow = final.buildGo118Module rec {
+                pname = "gow";
+                version = "0.1.0";
+                src = gow-src;
+                vendorSha256 = "o6KltbjmAN2w9LMeS9oozB0qz9tSMYmdDW3CwUNChzA=";
+              };
+            })
+          ]; inherit system;
+        }; rec {
+        packages.default = buildGo118Module rec {
+          name = "CHANGEME";
+          pname = name;
+          src = ./.;
+          vendorSha256 = "";
+        };
 
-          devShells.default = mkShell { packages = [ go_1_18 gopls ]; };
-        }) // {
+        devShells.default = mkShell { packages = [ go_1_18 gopls gow ]; };
+      }) // {
       overlays.default = (final: _: {
         CHANGEME = self.packages."${final.system}".default;
       });
