63054bb000e6f1611d3259a22db4a2e3ca2199f4
Matthew Toohey <contact@mtoohey.com>
2022-06-13 22:05:27 -0400

    fix: follow inputs to poetry2nix

--- python/flake.nix
+++ python/flake.nix
@@ -4,16 +4,22 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
-    poetry2nix.url = "github:nix-community/poetry2nix";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.flake-utils.follows = "utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, utils, poetry2nix }:
     utils.lib.eachDefaultSystem
       (system:
-        let pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ poetry2nix.overlay ];
-        }; in
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ poetry2nix.overlay ];
+          };
+        in
         with pkgs; rec {
           packages.default = pkgs.poetry2nix.mkPoetryApplication {
             projectDir = ./.;
