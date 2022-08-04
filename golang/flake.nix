a77a322d9df47bc6fa97ed1f133d8f932c59f23d
Matthew Toohey <contact@mtoohey.com>
2022-08-03 21:21:19 -0400

    feat: wrap yaegi in the fixup phase

--- golang/flake.nix
+++ golang/flake.nix
@@ -33,19 +33,18 @@
             src = gow-src;
             vendorSha256 = "o6KltbjmAN2w9LMeS9oozB0qz9tSMYmdDW3CwUNChzA=";
           };
-          yaegi-rlwrapped =
-            let
-              yaegi = final.buildGoModule rec {
-                pname = "yaegi-rlwrapped";
-                version = yaegi-src.shortRev;
-                src = yaegi-src;
-                vendorSha256 = "pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
-                GOROOT = "${final.go_1_18}/share/go";
-              };
-            in
-            final.writeShellScriptBin "yaegi" ''
-              ${final.rlwrap}/bin/rlwrap ${yaegi}/bin/yaegi "$@"
+          yaegi-rlwrapped = final.buildGoModule rec {
+            pname = "yaegi-rlwrapped";
+            version = yaegi-src.shortRev;
+            src = yaegi-src;
+            vendorSha256 = "pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+            GOROOT = "${final.go_1_18}/share/go";
+            fixupPhase = ''
+              mv $out/bin/yaegi $out/bin/.yaegi-wrapped
+              printf "#!${final.bash}/bin/bash\n${final.rlwrap}/bin/rlwrap $out/bin/.yaegi-wrapped \"\$@\"\n" > $out/bin/yaegi
+              chmod +x $out/bin/yaegi
             '';
+          };
         })
         self.overlays.default
       ]; inherit system;
