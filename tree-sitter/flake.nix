8b09b410279cd81e8fe2b2c56e231a5cd7468b10
Matthew Toohey <contact@mtoohey.com>
2023-02-05 17:28:38 -0500

    feat(tree-sitter): add overlay and package output

--- tree-sitter/flake.nix
+++ tree-sitter/flake.nix
@@ -6,17 +6,28 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, utils, ... }: utils.lib.eachDefaultSystem
-    # TODO: add package output
-    (system: with import nixpkgs { inherit system; }; {
-      devShells.default = mkShell {
-        packages = [
-          nodejs
-          python3
-          tree-sitter
-          typescript
-          nodePackages.typescript-language-server
-        ];
+  outputs = { self, nixpkgs, utils }: {
+    overlays.default = _: prev: {
+      tree-sitter = prev.tree-sitter.override {
+        extraGrammars.tree-sitter-CHANGEME = {
+          language = "CHANGEME";
+          inherit (prev.tree-sitter) version;
+          src = _: ./.;
+        };
       };
-    });
+    };
+  } // utils.lib.eachDefaultSystem (system: with import nixpkgs
+    { overlays = [ self.overlays.default ]; inherit system; }; {
+    packages.default = tree-sitter.builtGrammars.tree-sitter-CHANGEME;
+
+    devShells.default = mkShell {
+      packages = [
+        nodejs
+        python3
+        tree-sitter
+        typescript
+        nodePackages.typescript-language-server
+      ];
+    };
+  });
 }
