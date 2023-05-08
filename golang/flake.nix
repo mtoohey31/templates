c03a1d2780f8c1e29cea5e41cf53887ff4bb1e48
Matthew Toohey <contact@mtoohey.com>
2023-05-08 17:54:16 -0400

    feat(golang): replace gow with watchexec

--- golang/flake.nix
+++ golang/flake.nix
@@ -18,30 +18,16 @@
   } // utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs {
-        overlays = [
-          (final: _: {
-            gow = final.buildGoModule rec {
-              pname = "gow";
-              version = builtins.substring 0 7 src.rev;
-              src = final.fetchFromGitHub {
-                owner = "mitranim";
-                repo = pname;
-                rev = "36c8536a96b851631e800bb00f73383fc506f210";
-                sha256 = "q56s97j+Npurb942TeQhJPqq1vl/XFe7a2Dj5fw7EtQ=";
-              };
-              vendorSha256 = "o6KltbjmAN2w9LMeS9oozB0qz9tSMYmdDW3CwUNChzA=";
-            };
-          })
-          self.overlays.default
-        ]; inherit system;
+        overlays = [ self.overlays.default ];
+        inherit system;
       };
-      inherit (pkgs) CHANGEME go gopls gow mkShell;
+      inherit (pkgs) CHANGEME go gopls mkShell watchexec;
     in
     {
       packages.default = CHANGEME;
 
       devShells.default = mkShell {
-        packages = [ go gopls gow ];
+        packages = [ go gopls watchexec ];
       };
     });
 }
