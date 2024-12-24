d1674f39d7149e28ffda3b75b39f2e1e1180a704
Matthew Toohey <contact@mtoohey.com>
2024-12-24 16:04:52 -0500

    empty: remove C stuff

--- empty/nix/flake.nix
+++ empty/nix/flake.nix
@@ -6,27 +6,12 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }: {
-    overlays.default = final: _: {
-      CHANGEME = final.callPackage
-        ({ stdenv }: stdenv.mkDerivation {
-          pname = "CHANGEME";
-          version = "0.1.0";
-          src = builtins.path { path = ./..; name = "CHANGEME-src"; };
-        })
-        { };
-    };
-  } // utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs {
-        overlays = [ self.overlays.default ];
-        inherit system;
-      };
-      inherit (pkgs) CHANGEME mkShell;
+      pkgs = import nixpkgs { inherit system; };
+      inherit (pkgs) mkShell;
     in
     {
-      packages.default = CHANGEME;
-
-      devShells.default = mkShell { inputsFrom = [ CHANGEME ]; };
+      devShells.default = mkShell { packages = [ ]; };
     });
 }
